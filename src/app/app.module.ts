3950ba26235447875faae56b4330f21e011d400b
Metapyroxia <jade.k.beesley@gmail.com>
2018-06-02 19:11:46 +0100

    Enabled router. Generated story view component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
@@ -8,12 +9,16 @@ import { MaterialModule } from "./material/material.module";
 
 import { LandingScreenComponent } from "./components/landing-screen/landing-screen.component";
 import { environment } from "../environments/environment.prod";
+import { StoryViewComponent } from "./components/story-view/story-view.component";
+
+const appRoutes: Routes = [{ path: "", component: LandingScreenComponent }];
 
 @NgModule({
-	declarations: [AppComponent, LandingScreenComponent],
+	declarations: [AppComponent, LandingScreenComponent, StoryViewComponent],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
+		RouterModule.forRoot(appRoutes, { enableTracing: false }),
 		MaterialModule,
 		HttpClientModule
 	],
---
+++ src/app/components/story-view/story-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-story-view",
+	templateUrl: "./story-view.component.html",
+	styleUrls: ["./story-view.component.scss"]
+})
+export class StoryViewComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
